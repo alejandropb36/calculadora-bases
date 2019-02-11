90b4aa67d59cd63793e1c5defb31518a8f9ba49c
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-02-10 21:17:09 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int opc = 0;
+    int baseN, baseM;
+    char numero[100];
+
+    do
+    {
+        cout << "------------ Calculadora de bases---------" << endl;
+        cout << endl;
+        cout << "Ingresa  "
+
+    }while(opc != '0');
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
